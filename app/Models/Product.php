33512ec8393185fecea29d39cc70305672b189ca
Tanpaisen <tinh7demtb@gmail.com>
2026-09-20 18:31:28 +0700

    Fix loi merge  #39

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,8 +9,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Facades\Cache;
-
 
 class Product extends Model
 {
